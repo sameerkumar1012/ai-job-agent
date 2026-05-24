285c4b775801486bd31dfc47323cb9d4a0631530
Sameer Kumar <142688169+sameerkumar1012@users.noreply.github.com>
2026-05-25 00:41:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ gemini_model = genai.GenerativeModel(
 # ===================================================
 
 groq_llm = ChatGroq(
-    groq_api_key=GROQ_API_KEY,
+    api_key = GROQ_API_KEY,
     model_name="llama-3.3-70b-versatile",
     temperature=0.3
 )
